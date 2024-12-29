3f887d77d3d389324db00c77ccc32716c087cd47
PiyushBedre45 <113894461+PiyushBedre45@users.noreply.github.com>
2024-12-29 17:17:54 +0530

    f

--- src/app/_components/navbar.js
+++ src/app/_components/navbar.js
@@ -9,25 +9,25 @@ const Navbar = () => {
                 <div className="w-[95%] mx-auto flex flex-wrap justify-between md:justify-evenly items-center py-4 md:py-6">
                     <h1 className="text-2xl md:text-3xl font-bold text-white">Uzbekistan Medi</h1>
                     <div className="flex flex-wrap gap-4 md:gap-6 mt-2 lg:mt-0">
-                        <Link href={'#'}>
+                        <Link href='#'>
                             <h1 className="text-base md:text-lg text-white hover:text-gray-300 cursor-pointer">Home</h1>
                         </Link>
-                        <Link href={'#'}>
+                        <Link href='#'>
                             <h1 className="text-base md:text-lg text-white hover:text-gray-300 cursor-pointer">About</h1>
                         </Link>
-                        <Link href={'#'}>
+                        <Link href='#'>
                             <h1 className="text-base md:text-lg text-white hover:text-gray-300 cursor-pointer">MBBS in Uzbekistan</h1>
                         </Link>
-                        <Link href={'#'}>
+                        <Link href='#'>
                             <h1 className="text-base md:text-lg text-white hover:text-gray-300 cursor-pointer">Universities</h1>
                         </Link>
-                        <Link href={'#'}>
+                        <Link href='#'>
                             <h1 className="text-base md:text-lg text-white hover:text-gray-300 cursor-pointer">Blogs</h1>
                         </Link>
-                        <Link href={'#'}>
+                        <Link href='#'>
                             <h1 className="text-base md:text-lg text-white hover:text-gray-300 cursor-pointer">Gallery</h1>
                         </Link>
-                        <Link href={'#'}>
+                        <Link href='#'>
                             <h1 className="text-base md:text-lg text-white hover:text-gray-300 cursor-pointer">Contact Us</h1>
                         </Link>
                     </div>
