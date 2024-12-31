0cc6766b8c6eed43bbb4abd053b2df558d55fdfe
PiyushBedre45 <113894461+PiyushBedre45@users.noreply.github.com>
2024-12-31 15:31:14 +0530

    slider added

--- src/app/_components/galary.js
+++ src/app/_components/galary.js
@@ -1,32 +1,44 @@
 import React from 'react'
 
-
 const Galary = () => {
     return (
         <>
-            <div className='w-[80%] mx-auto mt-[100px]'>
+            <div className='w-[75%] mx-auto mt-[100px]'>
                 <h1 className="text-4xl font-bold text-center">
                     Recent Photo <span className="text-[#0da9b0] underline"> Gallery</span>
                 </h1>
                 <p className="text-center mt-2">
                     Celebrating Achievements, One Click at a Time!
                 </p>
-                <div className='flex flex-wrap items-center justify-center gap-14  mt-10'>
-                    <img
-                        src="https://media.istockphoto.com/id/1312706413/photo/modern-hospital-building.jpg?s=2048x2048&w=is&k=20&c=15TsJBPquZtgf8ciMtV6wlEAccnI5RJoNcqzFY9qe80="
-                        alt="Logo"
-                        className='w-[350px] h-fit rounded-lg'
-                    />
-                    <img
-                        src="https://media.istockphoto.com/id/1312706413/photo/modern-hospital-building.jpg?s=2048x2048&w=is&k=20&c=15TsJBPquZtgf8ciMtV6wlEAccnI5RJoNcqzFY9qe80="
-                        alt="Logo"
-                        className='w-[350px] h-fit rounded-lg'
-                    />
-                    <img
-                        src="https://media.istockphoto.com/id/1312706413/photo/modern-hospital-building.jpg?s=2048x2048&w=is&k=20&c=15TsJBPquZtgf8ciMtV6wlEAccnI5RJoNcqzFY9qe80="
-                        alt="Logo"
-                        className='w-[350px] h-fit rounded-lg'
-                    />
+                <div className="mt-10 overflow-x-auto scrollbar-hide">
+                    <div className="flex gap-10 w-max">
+                        <img
+                            src="https://media.istockphoto.com/id/1312706413/photo/modern-hospital-building.jpg?s=2048x2048&w=is&k=20&c=15TsJBPquZtgf8ciMtV6wlEAccnI5RJoNcqzFY9qe80="
+                            alt="Logo"
+                            className='w-[350px] h-fit rounded-lg'
+                        />
+                        <img
+                            src="https://media.istockphoto.com/id/1312706413/photo/modern-hospital-building.jpg?s=2048x2048&w=is&k=20&c=15TsJBPquZtgf8ciMtV6wlEAccnI5RJoNcqzFY9qe80="
+                            alt="Logo"
+                            className='w-[350px] h-fit rounded-lg'
+                        />
+                        <img
+                            src="https://media.istockphoto.com/id/1312706413/photo/modern-hospital-building.jpg?s=2048x2048&w=is&k=20&c=15TsJBPquZtgf8ciMtV6wlEAccnI5RJoNcqzFY9qe80="
+                            alt="Logo"
+                            className='w-[350px] h-fit rounded-lg'
+                        />
+                        <img
+                            src="https://media.istockphoto.com/id/1312706413/photo/modern-hospital-building.jpg?s=2048x2048&w=is&k=20&c=15TsJBPquZtgf8ciMtV6wlEAccnI5RJoNcqzFY9qe80="
+                            alt="Logo"
+                            className='w-[350px] h-fit rounded-lg'
+                        />
+                        <img
+                            src="https://media.istockphoto.com/id/1312706413/photo/modern-hospital-building.jpg?s=2048x2048&w=is&k=20&c=15TsJBPquZtgf8ciMtV6wlEAccnI5RJoNcqzFY9qe80="
+                            alt="Logo"
+                            className='w-[350px] h-fit rounded-lg'
+                        />
+
+                    </div>
                 </div>
             </div>
         </>
--- src/app/_components/servicies.js
+++ src/app/_components/servicies.js
@@ -7,37 +7,37 @@ const Services = () => {
             <div className='bg-teal-50 mt-[40px] lg:mt-[100px] '>
                 <h1 className='text-4xl font-bold text-center py-[60px]'>Our Valuable <span className='text-[#0da9b0] underline'>Services</span></h1>
                 <div className='flex gap-5 flex-wrap w-[80%] mx-auto items-center justify-center'>
-                    <div className='bg-white flex flex-col rounded-md gap-2 items-center justify-center p-8 w-[320px] shadow-2xl'>
+                    <div className='bg-white flex flex-col rounded-md gap-2 items-center justify-center p-8 w-[320px] shadow-2xl border border-[#e0e0e0]'>
                         <FaSchool className='text-[#0da9b0] font-bold text-6xl' />
                         <h1 className='text-2xl font-semibold text-teal-700'>Admissoion</h1>
                         <p className='text-center'>Get the guaranteed admission to
                             top university</p>
                     </div>
-                    <div className='bg-white flex flex-col rounded-md gap-2 items-center justify-center p-8 w-[320px] shadow-2xl'>
+                    <div className='bg-white flex flex-col rounded-md gap-2 items-center justify-center p-8 w-[320px] shadow-2xl border border-[#e0e0e0]'>
                         <FaSchool className='text-[#0da9b0] font-bold text-6xl' />
                         <h1 className='text-2xl font-semibold text-teal-700'>Admissoion</h1>
                         <p className='text-center'>Get the guaranteed admission to
                             top university</p>
                     </div>
-                    <div className='bg-white flex flex-col rounded-md gap-2 items-center justify-center p-8 w-[320px] shadow-2xl'>
+                    <div className='bg-white flex flex-col rounded-md gap-2 items-center justify-center p-8 w-[320px] shadow-2xl border border-[#e0e0e0]'>
                         <FaSchool className='text-[#0da9b0] font-bold text-6xl' />
                         <h1 className='text-2xl font-semibold text-teal-700'>Admissoion</h1>
                         <p className='text-center'>Get the guaranteed admission to
                             top university</p>
                     </div>
-                    <div className='bg-white flex flex-col rounded-md gap-2 items-center justify-center p-8 w-[320px] shadow-2xl'>
+                    <div className='bg-white flex flex-col rounded-md gap-2 items-center justify-center p-8 w-[320px] shadow-2xl border border-[#e0e0e0]'>
                         <FaSchool className='text-[#0da9b0] font-bold text-6xl' />
                         <h1 className='text-2xl font-semibold text-teal-700'>Admissoion</h1>
                         <p className='text-center'>Get the guaranteed admission to
                             top university</p>
                     </div>
-                    <div className='bg-white flex flex-col rounded-md gap-2 items-center justify-center p-8 w-[320px] shadow-2xl'>
+                    <div className='bg-white flex flex-col rounded-md gap-2 items-center justify-center p-8 w-[320px] shadow-2xl border border-[#e0e0e0]'>
                         <FaSchool className='text-[#0da9b0] font-bold text-6xl' />
                         <h1 className='text-2xl font-semibold text-teal-700'>Admissoion</h1>
                         <p className='text-center'>Get the guaranteed admission to
                             top university</p>
                     </div>
-                    <div className='bg-white flex flex-col rounded-md gap-2 items-center justify-center p-8 w-[320px] shadow-2xl'>
+                    <div className='bg-white flex flex-col rounded-md gap-2 items-center justify-center p-8 w-[320px] shadow-2xl border border-[#e0e0e0]'>
                         <FaSchool className='text-[#0da9b0] font-bold text-6xl' />
                         <h1 className='text-2xl font-semibold text-teal-700'>Admissoion</h1>
                         <p className='text-center'>Get the guaranteed admission to
