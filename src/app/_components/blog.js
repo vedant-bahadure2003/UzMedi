9249d98109cf3c90e57885202985beabef31782e
PiyushBedre45 <113894461+PiyushBedre45@users.noreply.github.com>
2024-12-31 16:32:19 +0530

    done

--- src/app/_components/blog.js
+++ src/app/_components/blog.js
@@ -7,17 +7,17 @@ const Blog = () => {
 
     return (
         <>
-            <div className="w-[80%] mx-auto mt-[100px]">
+            <div className="w-[90%] lg:w-[80%] mx-auto mt-[100px]">
                 <h1 className="text-4xl font-bold text-center">
                     Our Latest <span className="text-[#0da9b0] underline">Blog</span>
                 </h1>
                 <p className="text-center mt-2">
                     Empowering Future Doctors with Valuable Information!
                 </p>
-                <div className="flex overflow-x-auto gap-10 mt-10 scrollbar-hide ">
+                <div className="flex overflow-x-auto gap-10 mt-10 scrollbar-hide">
                     {blogDetails.map((blog) => (
-                        <div key={blog.id} className="w-[300px] flex-shrink-0">
-                            <div className="w-[300px] border border-gray-200 shadow-xl rounded-lg overflow-hidden">
+                        <div key={blog.id} className="w-[290px] lg:w-[300px]   flex-shrink-0">
+                            <div className="w-[290px] lg:w-[300px] border border-gray-200 shadow-xl rounded-lg overflow-hidden">
                                 <div>
                                     <img
                                         src={blog.blogImage}
